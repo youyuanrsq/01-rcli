838f91f675913f4c1696888e319f671fefea6cc9
fredrick_rsq <1140623243@qq.com>
2024-04-20 23:44:53 +0800

    support generic csv to json and yaml output

--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,10 @@
 use anyhow::{Ok, Result};
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -15,16 +18,26 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    // headers.iter() -> 使用 headers 的迭代器
+    // record.iter() -> 使用 record 的迭代器
+    // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
+    // collect::<Value>() -> 将元组的迭代器转换为 JSON Value
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
 
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    fs::write(output, content)?;
 
     Ok(())
 }
